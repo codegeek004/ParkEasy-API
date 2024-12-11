d252c106a3eaf20c8313d6489d26b4b270fa603d
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-11 09:02:59 +0530

    modified the default user model and defined custom fields where email is the unique field

--- parkeasy/models.py
+++ parkeasy/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+class UserManager(BaseUserManager):
+	def create_user(self, email, name, tc, password=None, passwor2=None):
+		"""
+		creates and saves the user with email, name, tc and password
+		"""
+		if not email:
+			raise ValueError("You must have an email address")
+
+		user = self.model(
+				email = self.normalize_email(email),
+				name = name,
+				tc=tc
+			)
+		user.save_password(password)
+		#saves into a database instance
+		user.save(using=self._db)
+
+	def create_superuser(self, email, name, tc, password=None, passwor2=None):
+		if not email:
+			raise ValueError("You must have an email address")
+		user = self.create_user(
+				email,
+				password=password,
+				name=name,
+				tc=tc 
+			)
+		user.is_admin=True 
+		user.save(using=self._db)
+		return user
+
+class User(AbstractBaseUser):
+	email = models.EmailField(verbose_name="email", max_length=255, unique=True)
+	tc = models.BooleanField()
+	is_active = models.BooleanField(default=True)
+	is_admin = models.BooleanField(default=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	objects = UserManager()
+
+	USERNAME_FIELD = "email"
+	REQUIRED_FIELDS = ["name","tc"]
+
+	def __str__(self):
+		return self.email 
+
+	
+
+
+
