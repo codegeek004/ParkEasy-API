de516050408f515aedf734c7503dea007733eb27
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-12 17:22:45 +0530

    authorization added for vehicle view

--- parkeasy/views.py
+++ parkeasy/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from django.http import HttpResponse
+from parkeasy.serializers import RegisterSerializer, LoginSerializer, VehicleSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.generics import ListAPIView
+from .models import *
+from django.contrib.auth.decorators import login_required  
+from django.utils.decorators import method_decorator 
 
-# Create your views here.
+class HomeView(APIView):
+    def get(self, request):
+        return Response({"message" : "Welcome to the JWT API"})
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = authenticate(username=serializer.validated_data['username'],
+                                password=serializer.validated_data['password'])
+            if user is not None:
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                        'access' : str(refresh.access_token),
+                        'refresh' : str(refresh)
+                    })
+            return Response({"error" : "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+class VehicleView(APIView):
+    permission_classes = [IsAuthenticated] 
+    def get(self, request):
+        vehicles = Vehicle.objects.all()
+        print(vehicles)
+        serializer = VehicleSerializer(vehicles, many=True)
+        print(serializer.data)
+        return Response(serializer.data)
+    def post(self, request, format=None):
+        serializer = VehicleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
