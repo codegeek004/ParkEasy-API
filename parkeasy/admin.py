e67a7e46907bc8bf5fdebdc52a740ecea8515516
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-12 17:25:05 +0530

    admin interface

--- parkeasy/admin.py
+++ parkeasy/admin.py
@@ -1,35 +1,35 @@
-from django.contrib import admin
-from parkeasy.models import User
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# from django.contrib import admin
+# from parkeasy.models import User
+# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-class UserModelAdmin(BaseUserAdmin):
-	#these fields override the definitions on the base UserModelAdmin
-	list_display = ["id", "email", "name", "tc", "is_admin", "is_staff"]
-	list_filter = ["is_admin", "is_staff", "is_active"]
+# class UserModelAdmin(BaseUserAdmin):
+# 	#these fields override the definitions on the base UserModelAdmin
+# 	list_display = ["id", "email", "name", "tc", "is_admin", "is_staff"]
+# 	list_filter = ["is_admin", "is_staff", "is_active"]
 
-	fieldsets = [
-		("User Credentials", {"fields" : ["email", "password"]}),
-		("Personal info", {"fields" : ["name", "tc"]}),
-		("Permissions", {"fields" : ["is_admin"]}),
-	]
+# 	fieldsets = [
+# 		("User Credentials", {"fields" : ["email", "password"]}),
+# 		("Personal info", {"fields" : ["name", "tc"]}),
+# 		("Permissions", {"fields" : ["is_admin"]}),
+# 	]
 
-	"""
-	Add fieldsets is not a standard ModelAdmin attribute.
-	UserModelAdmin overrides get_fieldsets to use this attribute when creating user
-	"""
-	add_fieldsets = [
-		#here none specifies no title or heading
-		(
-			None, 
-			{
-				"classes" : ["wide"],
-				"fields" : ["email", "name", "tc", "password1", "password2"]
-			},
-		),
-	]
+# 	"""
+# 	Add fieldsets is not a standard ModelAdmin attribute.
+# 	UserModelAdmin overrides get_fieldsets to use this attribute when creating user
+# 	"""
+# 	add_fieldsets = [
+# 		#here none specifies no title or heading
+# 		(
+# 			None, 
+# 			{
+# 				"classes" : ["wide"],
+# 				"fields" : ["email", "name", "tc", "password1", "password2", ]
+# 			},
+# 		),
+# 	]
 
-	search_fields = ["email"]
-	ordering = ["email","id"]
-	filter_horizontal = []
+# 	search_fields = ["email"]
+# 	ordering = ["email","id"]
+# 	filter_horizontal = []
 
-admin.site.register(User, UserModelAdmin)
+# admin.site.register(User, UserModelAdmin)
