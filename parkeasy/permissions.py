0407aa7037187859933f616423a5984b4e18e1ff
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-13 00:19:45 +0530

    added permission for user and admin

--- parkeasy/permissions.py
+++ parkeasy/permissions.py
@@ -2,8 +2,6 @@ from rest_framework.permissions import BasePermission
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role == 'admin'
-
-class IsUser(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.role == 'user' 
+        if request.user and request.user.is_authenticated:
+            return request.user.is_staff
+        return False
