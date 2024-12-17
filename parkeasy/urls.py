1147206898104a0ab74f82181263003cac58e851
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-17 16:48:33 +0530

    urls for 2 factor authentication

--- parkeasy/urls.py
+++ parkeasy/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     path('slots/', SlotView.as_view(), name="slots-list-create"),
     path('slots/<int:pk>/', SlotView.as_view(), name="slots-operation-id"),
     #Multi-Factor authentication
-    re_path(r'totp/create/$', TOTPCreateView.as_view(), name="totp-create"),
-    re_path(r'^totp/login/(?P<token>[0-9]{6})/$', TOTPVerifyView.as_view(), name="totp-login")
+    path('api/totp/create/', TOTPCreateView.as_view(), name='totp-create'),
+    path('api/totp/verify/', TOTPVerifyView.as_view(), name='totp-verify'),
+    path('api/login/', LoginWith2FAView.as_view(), name='login-with-2fa'),
+    # path('api/sensitive/', SensitiveView.as_view(), name='sensitive-api'),
     ]
