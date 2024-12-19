858930133ef46caa8fad42d4c4cab6bc65fe1939
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-19 14:50:46 +0530

    forget and reset password urls

--- parkeasy/urls.py
+++ parkeasy/urls.py
@@ -16,7 +16,9 @@ urlpatterns = [
     #Multi-Factor authentication
     path('api/totp/create/', TOTPCreateView.as_view(), name='totp-create'),
     path('api/totp/verify/', TOTPVerifyView.as_view(), name='totp-verify'),
-    # path('api/login/', LoginWith2FAView.as_view(), name='login-with-2fa'),
+    # path('api/login/', LoginWith2FAView.as_view(), name='login-with-2fa')
+    path('password/forgot/', ForgotPassword.as_view(), name='forgot-password'),
+    path('password/reset/<uidb64>/<token>/', ResetPassword.as_view(), name='reset-password'),
     #allauth
     
     ]
