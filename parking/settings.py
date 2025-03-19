0caa363222b5bd6c99c31b19b4b2db51038a4d24
codegeek004 <yashvaishnav1404@gmail.com>
2025-03-20 01:03:35 +0530

    increased access token timing to 120 minutes

--- parking/settings.py
+++ parking/settings.py
@@ -190,7 +190,7 @@ REST_FRAMEWORK = {
 
 }
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME" : timedelta(minutes=10),
+    "ACCESS_TOKEN_LIFETIME" : timedelta(minutes=120),
     "RERESH_TOKEN_LIFETIME" : timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,  
     "BLACKLIST_AFTER_ROTATION": True,   
