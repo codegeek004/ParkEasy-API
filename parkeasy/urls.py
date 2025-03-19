5851762f1eaa5b3fd994b91290139c2ea1dcd555
codegeek004 <yashvaishnav1404@gmail.com>
2025-03-19 11:10:24 +0530

    modified mfa path

--- parkeasy/urls.py
+++ parkeasy/urls.py
@@ -18,8 +18,8 @@ urlpatterns = [
     path('slots/', SlotView.as_view(), name="slots-list-create"),
     path('slots/<int:pk>/', SlotView.as_view(), name="slots-operation-id"),
     #Multi-Factor authentication
-    path('api/totp/create/', TOTPCreateView.as_view(), name='totp-create'),
-    path('api/totp/verify/', TOTPVerifyView.as_view(), name='totp-verify'),
+    path('api/mfa/create/', TOTPCreateView.as_view(), name='mfa-create'),
+    path('api/mfa/verify/', TOTPVerifyView.as_view(), name='mfa-verify'),
     # path('api/login/', LoginWith2FAView.as_view(), name='login-with-2fa')
     path('password/forgot/', ForgotPassword.as_view(), name='forgot-password'),
     path('password/reset/<uidb64>/<token>/', ResetPassword.as_view(), name='reset-password'),
