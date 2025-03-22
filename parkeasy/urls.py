d7f311ee90825c5e46e5454ab55b82c181a9ac52
codegeek004 <yashvaishnav1404@gmail.com>
2025-03-22 15:52:43 +0530

    modified password reset urls'

--- parkeasy/urls.py
+++ parkeasy/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('api/login/', LoginView.as_view(), name="login"),
     path('', HomeView.as_view(), name="home"),
     path('vehicles/', VehicleView.as_view(), name='vehicles'),
-    path('protected', ProtectedView.as_view(), name="home"),
+    path('protected', ProtectedView.as_view(), name="home"), # error with permissions
     path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('slots/', SlotView.as_view(), name="slots-list-create"),
     path('slots/<int:pk>/', SlotView.as_view(), name="slots-operation-id"),
@@ -17,8 +17,8 @@ urlpatterns = [
     path('api/mfa/create/', TOTPCreateView.as_view(), name='mfa-create'),
     path('api/mfa/verify/', TOTPVerifyView.as_view(), name='mfa-verify'),
     # path('api/login/', LoginWith2FAView.as_view(), name='login-with-2fa')
-    path('password/forgot/', ForgotPassword.as_view(), name='forgot-password'),
-    path('password/reset/<uidb64>/<token>/', ResetPassword.as_view(), name='reset-password'),
+    path('password/forgot/', ForgotPassword.as_view(), name='forgot-password'), # smtp error
+    path('password/reset/<uidb64>/<token>/', ResetPassword.as_view(), name='reset-password'), # smtp error
     path('iplogin/', LoginAPIView.as_view(), name='iplogin'),
 
 
